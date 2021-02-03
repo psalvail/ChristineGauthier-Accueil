806236f32461bf768e9fd14c3a5a9d6f97acff81
Philppe <philippe.salvail@christinegauthier.com>
2021-02-02 19:29:00 -0500

    working on NavBar

--- client/front-end/src/App.js
+++ client/front-end/src/App.js
@@ -1,9 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div>Hello World!</div>
+    <div>
+      <Navbar />
+    </div>
   );
 }
 
---
+++ client/front-end/src/components/Teams.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+const Teams = () => {
+  return;
+  <div>
+    <Navbar />
+  </div>;
+};
